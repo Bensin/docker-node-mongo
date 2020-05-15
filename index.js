515177439f20466302aa9ae48a78212fce5433c2
bensin <bensinpb@gmail.com>
2020-05-15 12:51:44 +1000

    updated

--- index.js
+++ index.js
@@ -9,16 +9,35 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname+"/public"));
 
+app.get('/todo', (req, res) => {
+    ToDo.find()
+      .then((toDos) => res.status(200).send(toDos))
+      .catch((err) => res.status(400).send(err));
+  });
+  
+  app.post('/todo', (req, res) => {
+    const body = req.body;
+    const toDo = new ToDo({
+      text: body.text,
+    });
+    toDo.save(toDo)
+      .then((savedToDo) => res.status(201).send(savedToDo))
+      .catch((err) => res.status(400).send(err));
+  });
+  
+  app.patch('/todo/:id', (req, res) => {
+    const { id } = req.params;
+    ToDo.findOneAndUpdate({ _id: id }, { done: true })
+      .then((toDo) => res.status(200).send(toDo))
+      .catch((err) => res.status(400).send(err));
+  });
 
 
 const mongoose = require('mongoose');
-const DB_URL = "mongo://mongo:27017/todoApp";
+const ToDo = require('./toDoModel.js').ToDo;
+const DB_URL = "mongodb://mongo:27017/toDoApp";
 
-app.listen(PORT,()=>{
-    console.log(`Listening on port`);
+mongoose.connect(DB_URL).then(()=>{
+    app.listen(PORT);
+    console.log(`Listening on port ${PORT}`);
 });
-
-// mongoose.connect(DB_URL).then(()=>{
-//     app.listen(PORT);
-//     console.log(`Listening on port`);
-// });
